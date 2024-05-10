5c72ed5f485b7e186706331bd93a9cb080a083a2
陈定涛 <dingtaochen@zhongkehuizhou.com>
2024-05-10 19:40:17 +0800

    cache: 2. 两数相加 js

--- 2.两数相加.js
+++ 2.两数相加.js
@@ -17,8 +17,20 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function(l1, l2) {
-
+var addTwoNumbers = function (l1, l2) {
+	let i = 0;
+	let carry = 0;
+	let result = [];
+	while (l1[i] !== undefined || l2[i] !== undefined || carry) {
+		let cur = (l1[i] ?? 0) + (l2[i] ?? 0) + carry;
+		let cur_res = cur % 10;
+		carry = Math.floor(cur / 10);
+		result.push(cur_res);
+		i++;
+	}
+	return resut;
 };
 // @lc code=end
 
+const res = addTwoNumbers([2, 4, 3], [5, 6, 4]);
+console.info("🚀 ~ res:", res);
